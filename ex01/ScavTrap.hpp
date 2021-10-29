6c1981de87357dbaa3a717672907e5084bef511e
Carmine Watcher <CWatcher@student.21-school.r>
2021-10-29 21:46:54 +0300

    + #pragma once

--- ex01/ScavTrap.hpp
+++ ex01/ScavTrap.hpp
@@ -1,10 +1,12 @@
+#pragma once
+
 #include "ClapTrap.hpp"
 #include <string>
 
 class ScavTrap: public ClapTrap
 {
 public:
-	ScavTrap( std::string const & name = "ScavTrap" );
+	ScavTrap( std::string const & name = "scavTrap" );
 	ScavTrap( ScavTrap const & );
 	~ScavTrap();
 	void				attack( std::string const & target );
